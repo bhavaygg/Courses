2b3618cef6db917c4c4d63d94a68d9d57400e114
chokerino <bhavay18384@iiitd.ac.in>
2022-07-02 18:44:25 -0700

    iiitd completed

--- IP/SUB/lab8/end.py
+++
@@ -1,9 +0,0 @@
-s1,s2 = input().split()
-n1= len(s1)
-n2 = len(s2)
-S1= s1.upper()
-S2= s2.upper()
-if(S1[n1-n2:]==S2 or S2[n2-n1:]==S1):
-	print(True)
-else: 
-	print(False)
--- ML/ass3/abcd.py
+++
@@ -1,8 +0,0 @@
-for i in [1,2]:
-for a in [1,2]:
-for a in [1,2]:
-for a in [1,2]:
-
-for a in [1,2]:
-
-
